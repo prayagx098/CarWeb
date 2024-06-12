7f36591a7827b8ddc8953c9d7c38bee3ddf5d9cc
prayag <prayagjishnu123@gmail.com>
2024-06-12 11:49:16 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        const navbar = document.getElementById('navbar');
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
